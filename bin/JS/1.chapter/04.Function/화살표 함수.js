d8ac500231c99d621e161a0f08f8fc29b26c8db1
YooEunJae327 <84011541+HollowFOV@users.noreply.github.com>
2021-09-30 13:51:34 +0900

    .

--- bin/JS/1.chapter/04.Function/화살표 함수.js
+++
@@ -1,8 +0,0 @@
-//화살표 함수는 일반적인 함수 선언과 다르게 화살표로 선언 합니다
-
-const add = (a,b) => 
-{
-    return a + b
-}
-
-console.log(add(1,2))
--- bin/JS/1.chapter/09.array-functions/unshift.js
+++
@@ -1,8 +0,0 @@
-//unshift
-//unshift는 shift의 반대이다
-//배열 맨 앞에 새 수나 문자를 추가한다
-
-let numbers = [10, 20, 30, 40]
-numbers.unshift(5)
-
-console.log(numbers)
